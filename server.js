c21a6f5e84cc32e7c7eab65a2845412ffa81fd57
Vansh <khuranavansh79@gmail.com>
2026-07-01 16:25:03 +0530

    homepage Added

--- server.js
+++ server.js
@@ -1,22 +1,24 @@
 const express = require("express");
-const cors = require("cors");
+const path = require("path");
 const dotenv = require("dotenv");
 
 dotenv.config();
 
 const app = express();
 
-app.use(cors());
-app.use(express.json());
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static("public"));
+app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("🚀 Barber Booking Server is Running...");
+    res.render("index");
 });
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+    console.log(`🚀 Waver Running on Port ${PORT}`);
 });
