5e7df510c93d78dbf6412d421f3c142d1befd0bb
Vansh <khuranavansh79@gmail.com>
2026-07-01 17:36:03 +0530

    add

--- server.js
+++ server.js
@@ -16,6 +16,9 @@ app.use(express.json());
 app.get("/", (req, res) => {
     res.render("index");
 });
+app.get("/login", (req, res) => {
+    res.render("login");
+});
 
 const PORT = process.env.PORT || 3000;
 
